ec9a91571d305fd9e53eb264ffe88e5a470ffee9
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 02:10:20 -0400

    Add `VerticalAlignment.orientation`

---
+++ Sources/AndroidSwiftUI/VerticalAlignment.swift
@@ -0,0 +1,24 @@
+//
+//  VerticalAlignment.swift
+//  AndroidSwiftUI
+//
+//  Created by Alsey Coleman Miller on 6/9/25.
+//
+
+import AndroidKit
+
+extension VerticalAlignment {
+    
+    var orientation: ViewGravity {
+        switch self {
+        case .top:
+            return .top
+        case .bottom:
+            return .bottom
+        case .center:
+            return .center
+        default:
+            return .noGravity
+        }
+    }
+}
