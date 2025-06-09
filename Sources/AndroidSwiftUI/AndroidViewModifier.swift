b9fadf392f1821d7404233d71555208656763fea
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 17:39:49 -0400

    Remove deprecated code

--- Sources/AndroidSwiftUI/AndroidViewModifier.swift
+++ Sources/AndroidSwiftUI/AndroidViewModifier.swift
@@ -11,3 +11,27 @@ public protocol AndroidViewModifier {
     
     func modifyAndroidView(_ view: AndroidView.View)
 }
+/*
+extension ModifiedContent: AndroidPrimitive where Content: View {
+
+    public var renderedBody: AnyView {
+        guard let viewModifier = modifier as? AndroidViewModifier else {
+            return AnyView(content)
+        }
+        let anyWidget: any AnyAndroidView
+        if let anyView = content as? AndroidPrimitive,
+           let _anyWidget = mapAnyView(
+            anyView.renderedBody,
+            transform: { (widget: AnyAndroidView) in widget }
+           )
+        {
+            anyWidget = _anyWidget
+        } else if let _anyWidget = content as? any AnyAndroidView {
+            anyWidget = _anyWidget
+        } else {
+            return AnyView(content)
+        }
+        
+    }
+}
+*/
