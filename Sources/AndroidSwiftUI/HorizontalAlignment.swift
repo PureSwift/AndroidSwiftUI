592c591e6141c78ae8ecddaf9d073b38e72ffbcd
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 02:10:29 -0400

    Add `HorizontalAlignment.orientation`

---
+++ Sources/AndroidSwiftUI/HorizontalAlignment.swift
@@ -0,0 +1,24 @@
+//
+//  HorizontalAlignment.swift
+//  AndroidSwiftUI
+//
+//  Created by Alsey Coleman Miller on 6/9/25.
+//
+
+import AndroidKit
+
+extension HorizontalAlignment {
+    
+    var orientation: ViewGravity {
+        switch self {
+        case .center:
+            return .center
+        case .leading:
+            return .left
+        case .trailing:
+            return .right
+        default:
+            return .noGravity
+        }
+    }
+}
