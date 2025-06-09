196b7998bd75b68f6b3adcde76a7d934057e1b9d
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 02:42:14 -0400

    Add `LinearLayoutView`

---
+++ Sources/AndroidSwiftUI/LinearLayout.swift
@@ -0,0 +1,77 @@
+//
+//  HStack.swift
+//  AndroidSwiftUI
+//
+//  Created by Alsey Coleman Miller on 6/9/25.
+//
+
+import AndroidKit
+
+protocol LinearLayoutView: LayoutContainerView {
+    
+    static var layoutOrientation: LinearLayout.Orientation { get }
+    
+    var gravity: ViewGravity { get }
+}
+
+extension LinearLayoutView {
+    
+    func createView(context: AndroidContent.Context) -> AndroidWidget.LinearLayout {
+        let view = AndroidWidget.LinearLayout(context)
+        updateView(view)
+        return view
+    }
+    
+    func updateView(_ view: AndroidWidget.LinearLayout) {
+        view.orientation = Self.layoutOrientation
+        view.gravity = self.gravity
+    }
+}
+
+extension VStack: AnyAndroidView {
+    
+    func createAndroidView(_ context: AndroidContent.Context) -> AndroidView.View {
+        createView(context: context)
+    }
+    
+    func updateAndroidView() {
+        
+    }
+    
+    func removeAndroidView() {
+        
+    }
+}
+
+extension VStack: LinearLayoutView {
+    
+    static var layoutOrientation: LinearLayout.Orientation { .vertical }
+    
+    var gravity: ViewGravity {
+        alignment.gravity
+    }
+}
+
+extension HStack: AnyAndroidView {
+    
+    func createAndroidView(_ context: AndroidContent.Context) -> AndroidView.View {
+        createView(context: context)
+    }
+    
+    func updateAndroidView() {
+        
+    }
+    
+    func removeAndroidView() {
+        
+    }
+}
+
+extension HStack: LinearLayoutView {
+    
+    static var layoutOrientation: LinearLayout.Orientation { .horizontal }
+    
+    var gravity: ViewGravity {
+        alignment.gravity
+    }
+}
