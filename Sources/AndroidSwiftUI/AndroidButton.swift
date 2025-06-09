9e485c9430a1240b3d8ea9e0144b78573a95b647
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 15:04:42 -0400

    Add `AndroidButton`

--- Sources/AndroidSwiftUI/AndroidButton.swift
+++ Sources/AndroidSwiftUI/AndroidButton.swift
@@ -7,77 +7,71 @@
 
 import AndroidKit
 
-extension Button: AnyAndroidView {
+extension _PrimitiveButtonStyleBody {
     
-    func createAndroidView(_ context: AndroidContent.Context) -> AndroidView.View {
-        let view: AndroidView.View
-        if let button = self as? Button<Text> {
-            view = button.createButtonView(context: context)
-        } else if let button = self as? Button<Image> {
-            view = button.createImageButtonView(context: context)
-        } else {
-            view = createContainerView(context: context)
-        }
-        // set on click listener
-        let listener = ViewOnClickListener()
-        listener.action = self.action
-        view.setClickable(true)
-        view.setOnClickListener(listener.as(AndroidView.View.OnClickListener.self))
-        return view
+    
+}
+
+/// SwiftUI view for `android.widget.Button`
+public struct AndroidButton {
+    
+    public typealias Label = Text
+    
+    let label: Label
+    
+    let action: () -> ()
+}
+
+public extension AndroidButton {
+    
+    init(
+      action: @escaping () -> (),
+      @ViewBuilder label: () -> Label
+    ) {
+      self.label = label()
+      self.action = action
     }
     
-    func updateAndroidView(_ view: AndroidView.View) {
-        if let button = self as? Button<Text>,
-           let view = view as? AndroidWidget.Button {
-            button.updateView(view)
-        } else if let button = self as? Button<Image>,
-            let view = view as? AndroidWidget.ImageButton {
-            button.updateView(view)
-        } else if let view = view as? LinearLayout {
-            self.updateContainerView(view)
-        }
-    }
-    
-    func removeAndroidView() {
-        
+    init<S>(
+        _ title: S,
+        action: @escaping () -> ()
+    ) where S: StringProtocol {
+        self.init(label: Text(title), action: action)
     }
 }
 
-extension Button where Label == Text {
+extension AndroidButton: AndroidViewRepresentable {
     
-    func createButtonView(context: AndroidContent.Context) -> AndroidWidget.Button {
+    public typealias Coordinator = Void
+    
+    /// Creates the view object and configures its initial state.
+    public func makeAndroidView(context: Self.Context) -> AndroidWidget.Button {
+        let view = AndroidWidget.Button(context.androidContext)
+        updateView(view)
+        return view
+    }
+    
+    /// Updates the state of the specified view with new information from SwiftUI.
+    public func updateAndroidView(_ view: AndroidWidget.Button, context: Self.Context) {
+        updateView(view)
+    }
+}
+
+extension AndroidButton {
+    
+    func createView(context: AndroidContent.Context) -> AndroidWidget.Button {
         let view = AndroidWidget.Button(context)
         updateView(view)
         return view
     }
     
     func updateView(_ view: AndroidWidget.Button) {
+        // update label
         self.label.updateTextView(view)
-    }
-}
-
-extension Button where Label == Image {
-    
-    func createImageButtonView(context: AndroidContent.Context) -> AndroidWidget.ImageButton {
-        let view = AndroidWidget.ImageButton(context)
-        updateView(view)
-        return view
-    }
-    
-    func updateView(_ view: AndroidWidget.ImageButton) {
-        self.label.updateImageView(view)
-    }
-}
-
-extension Button {
-    
-    func createContainerView(context: AndroidContent.Context) -> LinearLayout {
-        let view = AndroidWidget.LinearLayout(context)
-        updateContainerView(view)
-        return view
-    }
-    
-    func updateContainerView(_ view: LinearLayout) {
-        
+        // set on click listener
+        let listener = ViewOnClickListener()
+        listener.action = self.action
+        view.setClickable(true)
+        view.setOnClickListener(listener.as(AndroidView.View.OnClickListener.self))
     }
 }
