400b573a4d7e2c6afdc9e44557f3bbb016c66c70
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-06-09 16:47:21 -0400

    Add `AndroidLinearLayout`

--- Sources/AndroidSwiftUI/LinearLayout.swift
+++ Sources/AndroidSwiftUI/LinearLayout.swift
@@ -7,14 +7,57 @@
 
 import AndroidKit
 
-protocol LinearLayoutView: LayoutContainerView {
+/// Android Linear Layout
+public struct AndroidLinearLayout <Content> where Content : View {
     
-    static var layoutOrientation: LinearLayout.Orientation { get }
+    let orientation: Orientation
     
-    var gravity: ViewGravity { get }
+    let gravity: ViewGravity
+    
+    let content: Content
+    
+    public init(
+        orientation: LinearLayout.Orientation,
+        gravity: ViewGravity,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.orientation = orientation
+        self.gravity = gravity
+        self.content = content()
+    }
 }
 
-extension LinearLayoutView {
+public extension AndroidLinearLayout {
+    
+    /// Orientation
+    typealias Orientation = AndroidWidget.LinearLayout.Orientation
+}
+
+extension AndroidLinearLayout: ParentView {
+
+    public var children: [AnyView] {
+        (content as? GroupView)?.children ?? [AnyView(content)]
+    }
+}
+
+extension AndroidLinearLayout: AndroidViewRepresentable {
+    
+    public typealias Coordinator = Void
+    
+    /// Creates the view object and configures its initial state.
+    public func makeAndroidView(context: Self.Context) -> AndroidWidget.LinearLayout {
+        let view = AndroidWidget.LinearLayout(context.androidContext)
+        updateView(view)
+        return view
+    }
+    
+    /// Updates the state of the specified view with new information from SwiftUI.
+    public func updateAndroidView(_ view: AndroidWidget.LinearLayout, context: Self.Context) {
+        updateView(view)
+    }
+}
+
+extension AndroidLinearLayout {
     
     func createView(context: AndroidContent.Context) -> AndroidWidget.LinearLayout {
         let view = AndroidWidget.LinearLayout(context)
@@ -23,63 +66,31 @@ extension LinearLayoutView {
     }
     
     func updateView(_ view: AndroidWidget.LinearLayout) {
-        view.orientation = Self.layoutOrientation
+        view.orientation = self.orientation
         view.gravity = self.gravity
     }
 }
 
-extension VStack: AnyAndroidView {
+extension VStack: AndroidPrimitive {
     
-    public func createAndroidView(_ context: AndroidContent.Context) -> AndroidView.View {
-        createView(context: context)
-    }
-    
-    public func updateAndroidView(_ view: AndroidView.View) {
-        guard let linearLayout = view.as(LinearLayout.self) else {
-            assertionFailure()
-            return
+    var renderedBody: AnyView {
+        let proxy = _VStackProxy(self)
+        let gravity = proxy.subject._alignment.vertical.gravity
+        let linearLayout = AndroidLinearLayout(orientation: .vertical, gravity: gravity) {
+            proxy.subject.content
         }
-        updateView(linearLayout)
-    }
-    
-    public func removeAndroidView() {
-        
+        return AnyView(linearLayout)
     }
 }
 
-extension VStack: LinearLayoutView {
+extension HStack: AndroidPrimitive {
     
-    static var layoutOrientation: LinearLayout.Orientation { .vertical }
-    
-    var gravity: ViewGravity {
-        alignment.gravity
-    }
-}
-
-extension HStack: AnyAndroidView {
-    
-    public func createAndroidView(_ context: AndroidContent.Context) -> AndroidView.View {
-        createView(context: context)
-    }
-
-    public func updateAndroidView(_ view: AndroidView.View) {
-        guard let linearLayout = view.as(LinearLayout.self) else {
-            assertionFailure()
-            return
+    var renderedBody: AnyView {
+        let proxy = _HStackProxy(self)
+        let gravity = proxy.subject._alignment.vertical.gravity
+        let linearLayout = AndroidLinearLayout(orientation: .horizontal, gravity: gravity) {
+            proxy.subject.content
         }
-        updateView(linearLayout)
-    }
-    
-    public func removeAndroidView() {
-        
-    }
-}
-
-extension HStack: LinearLayoutView {
-    
-    static var layoutOrientation: LinearLayout.Orientation { .horizontal }
-    
-    var gravity: ViewGravity {
-        alignment.gravity
+        return AnyView(linearLayout)
     }
 }
