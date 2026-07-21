e01ecc4869e66c92b8fe4fec2f331755d521641e
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-21 19:37:22 -0400

    Add Material Components dependency

--- Demo/app/build.gradle.kts
+++ Demo/app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.fragment)
+    implementation(libs.google.material)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
