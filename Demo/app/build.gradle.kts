6d8c37b73df0d66a7372087fdca24a6ba8a8ac4b
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-21 15:11:29 -0400

    Add androidx.fragment dependency

--- Demo/app/build.gradle.kts
+++ Demo/app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.fragment)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
