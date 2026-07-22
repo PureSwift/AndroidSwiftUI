4ed55877ab69c96b525f3e3b1900d437e2edc5a2
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-22 08:53:48 -0400

    Remove the ten child limit from view builders
    Parameter packs replace the fixed set of arity-specific buildBlock overloads,
    so a view builder now accepts any number of children.

--- Sources/AndroidSwiftUI/TokamakCore/Views/ViewBuilder.swift
+++ Sources/AndroidSwiftUI/TokamakCore/Views/ViewBuilder.swift
@@ -118,128 +118,13 @@ public enum ViewBuilder {
 // swiftlint:disable function_parameter_count
 
 public extension ViewBuilder {
-  static func buildBlock<C0, C1>(_ c0: C0, _ c1: C1) -> TupleView<(C0, C1)>
-    where C0: View, C1: View
-  {
-    TupleView(c0, c1)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2
-  ) -> TupleView<(C0, C1, C2)> where C0: View, C1: View, C2: View {
-    TupleView(c0, c1, c2)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3
-  ) -> TupleView<(C0, C1, C2, C3)> where C0: View, C1: View, C2: View, C3: View {
-    TupleView(c0, c1, c2, c3)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4
-  ) -> TupleView<(C0, C1, C2, C3, C4)> where C0: View, C1: View, C2: View, C3: View, C4: View {
-    TupleView(c0, c1, c2, c3, c4)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4, C5>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4,
-    _ c5: C5
-  ) -> TupleView<(C0, C1, C2, C3, C4, C5)>
-    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View
-  {
-    TupleView(c0, c1, c2, c3, c4, c5)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4, C5, C6>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4,
-    _ c5: C5,
-    _ c6: C6
-  ) -> TupleView<(C0, C1, C2, C3, C4, C5, C6)>
-    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View
-  {
-    TupleView(c0, c1, c2, c3, c4, c5, c6)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4,
-    _ c5: C5,
-    _ c6: C6,
-    _ c7: C7
-  ) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7)>
-    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View
-  {
-    TupleView(c0, c1, c2, c3, c4, c5, c6, c7)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4,
-    _ c5: C5,
-    _ c6: C6,
-    _ c7: C7,
-    _ c8: C8
-  ) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8)>
-    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View
-  {
-    TupleView(c0, c1, c2, c3, c4, c5, c6, c7, c8)
-  }
-}
-
-public extension ViewBuilder {
-  static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8, C9>(
-    _ c0: C0,
-    _ c1: C1,
-    _ c2: C2,
-    _ c3: C3,
-    _ c4: C4,
-    _ c5: C5,
-    _ c6: C6,
-    _ c7: C7,
-    _ c8: C8,
-    _ c9: C9
-  ) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)>
-    where C0: View, C1: View, C2: View, C3: View, C4: View, C5: View, C6: View, C7: View, C8: View,
-    C9: View
-  {
-    TupleView(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)
+  /// Builds a block from an arbitrary number of child views.
+  ///
+  /// Parameter packs replace the fixed set of arity-specific overloads this file used to
+  /// declare, which capped a view builder at ten children.
+  static func buildBlock<each Content: View>(
+    _ content: repeat each Content
+  ) -> TupleView<(repeat each Content)> {
+    TupleView(views: repeat each content)
   }
 }
