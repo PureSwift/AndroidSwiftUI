002283068348919c91914847578ed281086c4314
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-23 12:11:23 -0400

    Add the full set of Alignment presets

--- AndroidSwiftUICore/Sources/AndroidSwiftUICore/Primitives/Stacks.swift
+++ AndroidSwiftUICore/Sources/AndroidSwiftUICore/Primitives/Stacks.swift
@@ -19,7 +19,13 @@ public struct Alignment: Sendable {
         self.vertical = vertical
     }
     public static let center = Alignment(horizontal: .center, vertical: .center)
+    public static let leading = Alignment(horizontal: .leading, vertical: .center)
+    public static let trailing = Alignment(horizontal: .trailing, vertical: .center)
+    public static let top = Alignment(horizontal: .center, vertical: .top)
+    public static let bottom = Alignment(horizontal: .center, vertical: .bottom)
     public static let topLeading = Alignment(horizontal: .leading, vertical: .top)
+    public static let topTrailing = Alignment(horizontal: .trailing, vertical: .top)
+    public static let bottomLeading = Alignment(horizontal: .leading, vertical: .bottom)
     public static let bottomTrailing = Alignment(horizontal: .trailing, vertical: .bottom)
 }
 
